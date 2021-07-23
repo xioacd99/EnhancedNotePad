1e840d65033f586025d2f5a2fb39cd4cbcd9e878
ducknew <2937521212@qq.com>
2021-07-23 23:50:42 +0800

    ui-

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ def test(filename):
 
 # FIXME: 实际查到的be有8个，但是只找到了6个，27、28两行不知道为什么查不到
 if __name__ == '__main__':
+    """
     ans = fileBurteForceFind('ENTest.txt', 'be')
     for singleResult in ans:
         print(singleResult)
+    """
+    lines=[]
+    with open('ENTest.txt') as f:
+       lines=f.readlines()
+    target='be'
+    for i,line in enumerate(lines):
+        idx=line.find(target)
+        if idx>=0:
+            print(f'{target} is in line:{i+1},pos:{idx+1}')
