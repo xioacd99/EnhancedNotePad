ecc4ad716c57cf005fb43601deba008ade708eaf
xioacd99 <1126995136@qq.com>
2021-07-23 23:07:13 +0800

    更改文件结构

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from stringFind import *
 if __name__ == '__main__':
     # ans = strKMPFind('being be are be ','be')
     # print(ans)
-    ans = fileKMPFind('ENTest.txt', 'be')
+    ans = fileKMPFind('dataset/ENTest.txt', 'be')
     for singleResult in ans:
         print(singleResult)
