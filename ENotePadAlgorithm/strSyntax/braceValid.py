ddbeaa3c3f04d8794ae5fcd10ad7f12961269a7f
xioacd99 <1126995136@qq.com>
2021-07-29 10:15:25 +0800

    括号匹配重构完成

--- ENotePadAlgorithm/strSyntax/braceValid.py
+++ ENotePadAlgorithm/strSyntax/braceValid.py
@@ -1,24 +1,22 @@
-def isBraceValid(str) -> bool:
-    symbols = ['(', ')', '[', ']', '{', '}']
+class SyntaxBrace(object):
+    def __init__(self):
+        self.symbols = ['(', ')', '[', ']', '{', '}']
+        self.pairs = {')': '(', ']': '[', '}': '{', }
 
-    pairs = {
-        ')': '(',
-        ']': '[',
-        '}': '{',
-    }
-
-    stk = list()
-    for ch in str:
-        if ch in symbols:
-            if ch in pairs:
-                if not stk or stk[-1] != pairs[ch]:
-                    return False
+    def isValid(self, str) -> bool:
+        stk = list()
+        for ch in str:
+            if ch in self.symbols:
+                if ch in self.pairs:
+                    if not stk or stk[-1] != self.pairs[ch]:
+                        return False
+                    else:
+                        stk.pop()
                 else:
-                    stk.pop()
-            else:
-                stk.append(ch)
-    return not stk
+                    stk.append(ch)
+        return not stk
+
 
 if __name__ == '__main__':
-    ans = isBraceValid('(hello, world)')
-    print(ans)
+    test = SyntaxBrace()
+    print(test.isValid('(hello, world)'))
